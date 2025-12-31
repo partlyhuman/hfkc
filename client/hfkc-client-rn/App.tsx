4cbfd8cf3d9d7b8200e5dcb2ea6f0ea28836c27b
Roger Braunstein <roger@partlyhuman.com>
2025-12-31 14:01:11 -0800

    Fighting with hot reload instances, adding mode changer to RN

--- client/hfkc-client-rn/App.tsx
+++ client/hfkc-client-rn/App.tsx
@@ -1,59 +1,33 @@
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
-import { useEffect, useState } from "react";
-import { hfkc, Mode } from "./HandsFreeKnitCounter";
-import { useAppState } from "./useAppState";
+import { Alert, Button, Linking, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { ConnectionState, HandsFreeKnitCounter, Mode } from "./HandsFreeKnitCounter";
 import { useEventListener } from "expo";
 import SegmentedControl from "@react-native-segmented-control/segmented-control";
 
 export default function App() {
-  // Wait for bluetooth to be available. Probably need to ask permissions too.
-  useEffect(() => {
-    const sub = hfkc.manager.onStateChange((state) => {
-      if (state === "PoweredOn") {
-        sub.remove();
-        hfkc.scanAndConnect().then();
-      }
-    }, true);
-    return () => {
-      sub.remove();
-      hfkc.disconnect().then();
-    };
-  }, []);
-
-  // Re-fetch on foreground
-  const appState = useAppState();
-  useEffect(() => {
-    if (appState === "active") {
-      hfkc.readCount().then();
-    }
-  }, [appState]);
-
-  // Map update events to states
+  const hfkc = HandsFreeKnitCounter.instance;
+  const [connectionState, setConnectionState] = useState<ConnectionState>();
+  useEventListener(hfkc.events, "connectionStateChanged", setConnectionState);
   const [mode, setMode] = useState(hfkc.mode);
   useEventListener(hfkc.events, "modeUpdate", setMode);
   const [count, setCount] = useState<number[]>([]);
   useEventListener(hfkc.events, "countUpdate", setCount);
-  const connected = count.length === 2;
 
   function onResetPressed() {
-    Alert.alert(
-      "Reset?",
-      "Resetting will clear the counter all the way back to zero. Are you sure?",
-      [
-        {
-          text: "Cancel",
-          style: "cancel",
-        },
-        {
-          text: "Reset",
-          style: "destructive",
-          onPress: () => hfkc.resetCount(),
-        },
-      ],
-    );
+    Alert.alert("Reset?", "Resetting will clear the counter all the way back to zero. Are you sure?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Reset",
+        style: "destructive",
+        onPress: () => hfkc.resetCount(),
+      },
+    ]);
   }
 
-  if (!connected) {
+  if (connectionState === "disconnected") {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>Connecting...</Text>
@@ -61,26 +35,42 @@ export default function App() {
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={[styles.text, styles.rowCount]}>Row {count[0]}</Text>
-      {mode === Mode.MODE_COUNT_ROW_STITCH && (
-        <Text style={[styles.text, styles.stitchCount]}>Stitch {count[1]}</Text>
-      )}
-      <View style={{ height: 50 }} />
-      <Button title={"Undo"} />
-      <Button title={"Restart Row"} onPress={() => hfkc.resetRow()} />
-      <Button title={"Reset"} color={"red"} onPress={onResetPressed} />
-      <SegmentedControl
-        values={["Row & Stitch", "Row"]}
-        selectedIndex={mode}
-        onChange={(event) => {
-          const newMode = event.nativeEvent.selectedSegmentIndex as Mode;
-          hfkc.setMode(newMode).then();
-        }}
-      />
-    </View>
-  );
+  if (connectionState === "bluetoothDisabled") {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Bluetooth not enabled.</Text>
+        <Text>
+          Bluetooth is required to connect to the device. Check your device settings including the Control Center.
+        </Text>
+        <Button title={"Go to Settings"} onPress={() => Linking.openSettings()} />
+      </View>
+    );
+  }
+
+  if (connectionState === "connected") {
+    return (
+      <View style={styles.container}>
+        <Text style={[styles.text, styles.rowCount]}>Row {count[0]}</Text>
+        {mode === Mode.MODE_COUNT_ROW_STITCH && (
+          <Text style={[styles.text, styles.stitchCount]}>Stitch {count[1]}</Text>
+        )}
+        <View style={{ height: 50 }} />
+
+        <Button title={"Undo"} />
+        <Button title={"Restart Row"} onPress={() => hfkc.resetRow()} />
+        <Button title={"Reset"} color={"red"} onPress={onResetPressed} />
+        <SegmentedControl
+          style={styles.modeSegmentedControl}
+          values={["Row & Stitch", "Row"]}
+          selectedIndex={mode}
+          onChange={(event) => {
+            const newMode = event.nativeEvent.selectedSegmentIndex as Mode;
+            hfkc.setMode(newMode).then();
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -95,4 +85,7 @@ const styles = StyleSheet.create({
   },
   rowCount: {},
   stitchCount: {},
+  modeSegmentedControl: {
+    width: "80%",
+  },
 });
--- client/hfkc-client-rn/HandsFreeKnitCounter.ts
+++ client/hfkc-client-rn/HandsFreeKnitCounter.ts
@@ -1,19 +1,7 @@
-import {
-  BleError,
-  BleManager,
-  Characteristic,
-  Device,
-  Subscription,
-} from "react-native-ble-plx";
+import { BleError, BleManager, Characteristic, Device, LogLevel, State, Subscription } from "react-native-ble-plx";
 import { EventEmitter } from "expo";
-import {
-  fromBase64,
-  packCounts,
-  packMode,
-  toBase64,
-  unpackCounts,
-  unpackMode,
-} from "./binaryUtils";
+import { fromBase64, packCounts, packMode, toBase64, unpackCounts, unpackMode } from "./binaryUtils";
+import { AppState, AppStateStatus, PermissionsAndroid, Platform } from "react-native";
 
 const DEVICE_NAME = "Hands-Free Knit Counter";
 const SERVICE_HKFC = "49feaff1-039c-4702-b544-6d50dde1e1a1";
@@ -25,59 +13,150 @@ export enum Mode {
   MODE_COUNT_ROW = 1,
 }
 
+export type ConnectionState = undefined | "bluetoothDisabled" | "disconnected" | "connected";
+
 type EventMap = {
   countUpdate: (count: number[]) => void;
   modeUpdate: (mode: Mode) => void;
+  connectionStateChanged: (state: ConnectionState) => void;
+};
+
+// The idea of this is to survive dev reloads
+const SINGLETON_KEY = Symbol.for("HandsFreeKnitCounter Singleton");
+const globalThisExt = globalThis as typeof globalThis & {
+  [SINGLETON_KEY]?: HandsFreeKnitCounter;
 };
 
 export class HandsFreeKnitCounter {
-  public readonly manager = new BleManager();
   public readonly events = new EventEmitter<EventMap>();
 
+  private readonly manager = new BleManager();
   private device: Device | undefined;
-  private subscription: Subscription | undefined;
+  private subscriptions: Subscription[] = [];
   private counts: number[] = [];
+  private _connectionState: ConnectionState;
+  public get connectionState() {
+    return this._connectionState;
+  }
 
   private _mode: Mode = Mode.MODE_COUNT_ROW_STITCH;
   public get mode() {
     return this._mode;
   }
 
-  public async scanAndConnect() {
-    console.log("scanning...");
-    await this.manager.startDeviceScan(
-      [SERVICE_HKFC],
-      null,
-      async (error, device) => {
-        console.log("found", device);
-        if (error || !device) {
-          console.error(error);
-          return;
+  static get instance() {
+    return (globalThisExt[SINGLETON_KEY] ??= new HandsFreeKnitCounter());
+  }
+
+  constructor() {
+    console.log("----------------------");
+    this.onDisconnected = this.onDisconnected.bind(this);
+    this.onCharacteristicUpdate = this.onCharacteristicUpdate.bind(this);
+    this.manager.setLogLevel(LogLevel.Verbose).then();
+    this.events.addListener("connectionStateChanged", (c) => (this._connectionState = c));
+    this.getPermission().then(() => this.subscribeToHardwareStates());
+  }
+
+  private async subscribeToHardwareStates() {
+    let currentBluetoothState: State | undefined;
+    this.manager.onStateChange((state) => {
+      console.log("BT state", currentBluetoothState, "->", state);
+      if (state === currentBluetoothState) {
+        return;
+      } else {
+        currentBluetoothState = state;
+      }
+      if (state === "PoweredOn") {
+        this.events.emit("connectionStateChanged", "disconnected");
+        this.scanAndConnect().then();
+      } else {
+        this.events.emit("connectionStateChanged", "bluetoothDisabled");
+      }
+    }, true);
+    let currentAppState: AppStateStatus | undefined;
+    AppState.addEventListener("change", (appState) => {
+      console.log("app state", currentAppState, "->", appState);
+      if (appState === currentAppState) {
+        return;
+      } else {
+        currentAppState = appState;
+      }
+      if (appState === "active") {
+        if (this.connectionState === "connected") {
+          console.log("connected - refetching on foreground");
+          this.readCount().then();
+        } else if (this.connectionState === "disconnected") {
+          console.log("not connected - connecting");
+          this.scanAndConnect().then();
         }
-        this.manager.stopDeviceScan().then();
-        this.connectToDevice(device).then();
-      },
-    );
+      }
+    });
+  }
+
+  public async getPermission() {
+    if (Platform.OS === "ios") {
+      // I guess not needed for BTLE?
+      return true;
+    }
+    if (Platform.OS === "android" && PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION) {
+      const apiLevel = parseInt(Platform.Version.toString(), 10);
+
+      if (apiLevel < 31) {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      }
+      if (PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN && PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT) {
+        const result = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        ]);
+
+        return (
+          result["android.permission.BLUETOOTH_CONNECT"] === PermissionsAndroid.RESULTS.GRANTED &&
+          result["android.permission.BLUETOOTH_SCAN"] === PermissionsAndroid.RESULTS.GRANTED &&
+          result["android.permission.ACCESS_FINE_LOCATION"] === PermissionsAndroid.RESULTS.GRANTED
+        );
+      }
+    }
+    return false;
+  }
+
+  public async scanAndConnect() {
+    if (this.device && (await this.device.isConnected())) {
+      console.log("already connected.");
+      return;
+    }
+
+    console.log("scanning....");
+    await this.manager.startDeviceScan([SERVICE_HKFC], null, async (error, device) => {
+      console.log("found", device);
+      if (error || !device) {
+        console.error(error);
+        return;
+      }
+
+      console.log("no more scanning");
+      await this.manager.stopDeviceScan();
+      await this.connectToDevice(device);
+    });
   }
 
   private async connectToDevice(device: Device) {
     console.log("connecting...");
     this.device = await device.connect();
+    this.subscriptions.push(this.device.onDisconnected(this.onDisconnected));
     await this.device.discoverAllServicesAndCharacteristics(); // necessary?
-    this.subscription = this.device.monitorCharacteristicForService(
-      SERVICE_HKFC,
-      CHARACTERISTIC_ROW_STITCH,
-      this.onCharacteristicUpdate.bind(this),
+    this.subscriptions.push(
+      this.device.monitorCharacteristicForService(SERVICE_HKFC, CHARACTERISTIC_ROW_STITCH, this.onCharacteristicUpdate),
     );
     // Read current count & mode once at startup
-    this.readCount().then();
-    this.readMode().then();
+    await Promise.all([this.readMode(), this.readCount()]);
+    console.log("connected");
+    this.events.emit("connectionStateChanged", "connected");
   }
 
-  private onCharacteristicUpdate(
-    error: BleError | null,
-    characteristic: Characteristic | null,
-  ): void {
+  private onCharacteristicUpdate(error: BleError | null, characteristic: Characteristic | null): void {
     if (error || !characteristic || !characteristic.value) {
       console.error(error);
       return;
@@ -90,10 +169,7 @@ export class HandsFreeKnitCounter {
   }
 
   public async readCount() {
-    const c = await this.device?.readCharacteristicForService(
-      SERVICE_HKFC,
-      CHARACTERISTIC_ROW_STITCH,
-    );
+    const c = await this.device?.readCharacteristicForService(SERVICE_HKFC, CHARACTERISTIC_ROW_STITCH);
     if (c) {
       this.onCharacteristicUpdate(null, c);
     }
@@ -116,10 +192,7 @@ export class HandsFreeKnitCounter {
   }
 
   public async readMode() {
-    const c = await this.device?.readCharacteristicForService(
-      SERVICE_HKFC,
-      CHARACTERISTIC_MODE,
-    );
+    const c = await this.device?.readCharacteristicForService(SERVICE_HKFC, CHARACTERISTIC_MODE);
     if (c?.value) {
       const value = fromBase64(c.value);
       this._mode = unpackMode(new DataView(value));
@@ -140,11 +213,22 @@ export class HandsFreeKnitCounter {
     this.events.emit("modeUpdate", this._mode);
   }
 
-  public async disconnect() {
-    console.log("disconnecting...");
-    this.subscription?.remove();
-    this.device?.cancelConnection().then();
+  private onDisconnected() {
+    console.log("disconnected...");
+    this.events.emit("connectionStateChanged", "disconnected");
+    this.subscriptions.splice(0).map((s) => s.remove());
+    this.device = undefined;
+    // reconnect automatically?
+    console.log("reconnecting...");
+    this.scanAndConnect().then();
+  }
+
+  public async dispose() {
+    console.log("Disposing");
+    this.subscriptions.splice(0).map((s) => s.remove());
+    if (this.device) {
+      await this.manager.cancelDeviceConnection(this.device.id);
+    }
+    await this.manager.destroy();
   }
 }
-
-export const hfkc = new HandsFreeKnitCounter();
